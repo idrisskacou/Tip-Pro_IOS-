b70ffb7dfd493c63bdcbf90c4a6eedbe10ea08fe
Idriss Kacou <Idrisskacou@outlook.com>
2024-06-13 09:00:09 -0700

    Initial Commit

---
+++ Tip Pro/Tip_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_ProApp.swift
+//  Tip Pro
+//
+//  Created by idriss on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Tip_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
